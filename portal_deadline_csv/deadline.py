54cb596b449df945010a05a9b9fa88d5e55d1b15
nicothebrush <nicola.riolini@gmail.com>
2018-06-21 18:55:41 +0200

    generic bugfix (fast message)

--- portal_deadline_csv/deadline.py
+++ portal_deadline_csv/deadline.py
@@ -98,6 +98,7 @@ class PortalDeadline(osv.osv):
         self.unlink(cr, uid, deadline_ids, context=context) 
         # TODO;:
         user_id = 1
+        import pdb; pdb.set_trace()
         for row in rows:
             try:
                 line=row.split(';')
